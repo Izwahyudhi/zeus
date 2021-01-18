73e1a9aa968994e851b2dad992b9fe0f8bb8b786
root <izwahyudhi@github.com>
2021-01-18 13:39:14 +0700

    bebas

--- compile.sh
+++ compile.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-export KBUILD_BUILD_USER=Zeus_VIRGIN
+export KBUILD_BUILD_USER=Zeus_2x
 export KBUILD_BUILD_HOST=Izwahyudhi@github
 # Compile plox
 function compile() {
-    make -j$(nproc) O=out ARCH=arm64 lavender-perf_defconfig cc=clang-10
-    make -j$(nproc) O=out ARCH=arm64  cc=clang-10 \
+    make -j$(nproc) O=out ARCH=arm64 lavender-perf_defconfig cc=gcc-10
+    make -j$(nproc) O=out ARCH=arm64  cc=gcc-10 \
                                CROSS_COMPILE=aarch64-linux-gnu- \
                                CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 }
